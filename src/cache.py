37b97d0be42d08cc89b015aa908709e93e2bd42d
Phil Davis <phildavis17@gmail.com>
2026-01-10 00:17:58 -0500

    simplify

--- src/cache.py
+++ src/cache.py
@@ -8,16 +8,6 @@ from random import choice
 from typing import Any, Protocol
 
 
-class EvictionPolicyType(Enum):
-    LRU = "lru"
-    LFU = "lfu"
-    FIFO = "fifo"
-    MRU = "mru"
-    RANDOM = "random"
-
-class KeyType(Enum):
-    READABLE = "readable"
-    HASHED = "hashed"
 
 
 
@@ -38,7 +28,15 @@ class CacheMetadata:
     times_accessed: int
     hits: int
 
-
+    @classmethod
+    def new_metadata(cls):
+        now = get_utc_timestamp()
+        return CacheMetadata(
+            created_utc_timestamp = now,
+            last_accessed_utc_timestamp = now,
+            times_accessed = 0,
+            hits = 0,
+        )
 
 
 @dataclass
@@ -52,83 +50,22 @@ def get_utc_timestamp() -> float:
     return datetime.now(timezone.utc).timestamp()
 
 
-class EvictionPolicy(Protocol):
-    def get_next_key_to_delete(self, cache: dict[str, CachedCall]) -> str:
-        ...
+class JsonCache:
+    def __init__(
+        self,
+        cache_file_path: Path,
+        cache: dict | None
+    ):
+        self.cache_file_path = cache_file_path
+    
+    def _read_cache_file(self) -> dict:
+        with open(self.cache_file_path, "r") as cache_file:
+            file_contents = json.load(cache_file)
+        cache = file_contents.get("cache", {})
+        return cache
 
-def _get_least_recently_used_key(cache: dict[str, CachedCall]) -> str:
-    for key, cache_entry in cache.items():
+    def _write_cache_file(self) -> None:
+        with open(self.cache_file_path, "w") as cache_file:
+            json.dump(self.cache, cache_file)
+    
 
-
-
-class ColdHardCache:
-    def __init__(self):
-        self.cache: dict[str, CachedCall] = {}
-        self.eviction_policy: EvictionPolicy = None
-        self.config: CacheConfig = None
-
-    def store():
-        pass
-
-    def retrieve(self, key: str):
-        cache_entry = self.cache[key]
-        cache_entry.access_count += 1
-        cache_entry.utc_timestamp = get_utc_timestamp()
-        return cache_entry.value
-
-    def purge_expired(self):
-        for key in self.cache:
-            if not self._entry_is_current(key):
-                self.cache.pop(key)
-                # TODO: add log
-
-    def _entry_is_current(self, key) -> bool:
-        """
-        Given a key, returns True if that key's cache entry is olde
-        """
-        max_age = self.config.max_age_seconds
-        if max_age <= 0:
-            return True
-        current_time = get_utc_timestamp()
-        entry_timestamp = self.cache[key].utc_timestamp
-        entry_age = current_time - entry_timestamp
-        return entry_age > max_age
-
-    def clear(self):
-        self.cache = {}
-
-    def reset():
-        pass
-
-    def read_cache_file():
-        pass
-
-    def write_cache_file():
-        pass
-
-    @property
-    def least_recently_used_key():
-        pass
-
-    @property
-    def least_frequently_used_key():
-        pass
-
-    @property
-    def oldest_key():
-        pass
-
-    def __contains__(self, key: str) -> bool:
-        return key in self.cache
-
-    def __len__(self) -> int:
-        return len(self.cache)
-
-    def __repr__(self) -> str:
-        pass
-
-    def __enter__(self) -> None:
-        pass
-
-    def __exit__(self, *args, **kwargs):
-        pass
